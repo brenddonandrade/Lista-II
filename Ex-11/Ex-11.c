64bf7f3e5fe8b1ab16735e34bc2325722187ba82
oni <oni@pop-os.localdomain>
2022-01-23 10:35:40 -0300

    :package: feito Ex-11:

--- Ex-11/Ex-11.c
+++ Ex-11/Ex-11.c
@@ -15,51 +15,36 @@ struct dupla {
 	struct dupla *dprox;
 };
 
-struct dupla *cria(int n){
-	struct dupla *p, *ini, *ult;
-	float valor;
-	int i;
-
-	ult = NULL;
-
-	for(i=0 ; i<=n ; i++){
-		printf("\nInforme um valor: ");
-		scanf("%f", &valor);
-		p = (struct dupla*)malloc(sizeof(struct dupla));
-		p->info = valor;
-		p->eprox = NULL;
-		p->dprox = NULL;
-		if(ult){
-			ult->dprox = p;
-			p->eprox = ult;
-		}else 
-			ini = p;
-		ult = p;
-	}
-	return ini;
-}
-
-void imprime(struct dupla *p){
-	while(p){
-		printf("%f.2\t", p->info);
-		p = p->dprox;
-	}
-}
-
-struct dupla *insereFinal(struct dupla *p, int valor) {
-	struct dupla *q, *t;
+void imprima(struct dupla *p){
+	struct dupla *q;
 	
+	q = p;
+	puts("");
+	printf("Lista:\n");
+	while(q){
+
+		printf("%.2f\t", q->info);
+		q = q->dprox;
+	}
+	printf("\n");
+}
+
+
+struct dupla *insereFinal(struct dupla *p, float valor)
+{
+	struct dupla *q, *t;
 	q=(struct dupla*)malloc(sizeof(struct dupla));
-	q -> info = valor;
+ 	q->info = valor;
 	if(!p){
 		q->eprox = q->dprox = NULL;
 		return q;
-	}else {
+	}
+	else{
 		t=p;
-		while(t -> dprox)
-		t= t->dprox;
+		while(t->dprox)
+			t= t->dprox;
  		t->dprox = q;
- 		q->eprox = t;
+		q->eprox = t; 
 		q->dprox = NULL;
 		return p;
 	}
@@ -68,13 +53,18 @@ struct dupla *insereFinal(struct dupla *p, int valor) {
 struct dupla *removeK(struct dupla *p, int k){
 	struct dupla *q, *r, *t;
 	int cont;
+
+	if(!p){
+		printf("\nLista vazia.\n");
+		return NULL;
+	}
 	if(k==1){
-		if(!p-dprox){
+		if(!p->dprox){
 			free(p);
 			return NULL;
 		}else {
 			q = p;
-			p = p->dprox
+			p = p->dprox;
 			p->eprox = NULL;
 			free(q);
 			return p;
@@ -95,4 +85,86 @@ struct dupla *removeK(struct dupla *p, int k){
 		free(q);
 	}
 	return(p);
+}
+
+int busca(struct dupla *p, int valor){
+	struct dupla *aux;
+
+	aux = p;
+	while(aux){
+		if(aux->info == valor)
+			return 1;
+		aux = aux->dprox;
+	}
+	return 0;
+}
+
+
+int main() {
+	struct dupla *l1;
+	int  k, n, op, aux, cont=0;
+	float valor;
+
+	l1 = NULL;
+
+	
+
+		do{
+			puts("");
+			printf("\nDigite o que deseja fazer:\n1-Inserção \n2-Busca \n3-Remoção \n4-Imprimir lista \n5-Sair\n\n");
+			scanf("%d", &op);	
+			
+			switch(op){
+			case 1: 
+				printf("\nDigite o valor a ser incluido: ");
+				scanf("%f", &valor);
+				l1 = insereFinal(l1, valor);
+				cont++;
+				break;
+			
+			case 2:
+				if(!l1){
+					printf("\nLista vazia.\n\n");
+					break;
+				}
+
+				printf("\nDigite o valor buscado: ");
+				scanf("%f", &valor);
+				aux = busca(l1, valor);
+				if(aux==0)
+					printf("\nValor nao encontrado na lista.\n");
+				if(aux==1)
+					printf("\nValor encontrado.\n");
+				break;
+
+			case 3:
+				if(!l1){
+					printf("\nLista vazia.\n\n");
+					break;
+				}
+				printf("\nDigite qual no deseja remover: ");
+				scanf("%d", &n);
+				if(n<=cont){
+					l1 = removeK(l1, n);
+					printf("\nRemovido com sucesso.\n");
+				}else {
+					printf("\nLista nao possui tantos nos.\n");
+				}
+				break;
+
+			case 4:
+				imprima(l1);
+				break;
+
+			case 5:
+				printf("\nExit.\n");
+				break;
+
+			default:
+				printf("\nOpcao Invalida.\n");
+		}
+	}while(op!=5);
+
+	return 0;
+
 }
